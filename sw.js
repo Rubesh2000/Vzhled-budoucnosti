87d38242da3c4798a8683725c1bd7b1173636de2
Rubesh2000 <rubesh2000@icloud.com>
2026-05-08 20:02:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,20 @@
-const CACHE="future-sense-mvp-v1";
+const CACHE_NAME="future-sense-v01";
+const FILES=[
+"./",
+"./index.html",
+"./manifest.webmanifest",
+"./icon-192.png",
+"./icon-512.png"
+];
+
 self.addEventListener("install",e=>{
-e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon-192.png"])));
+e.waitUntil(
+ caches.open(CACHE_NAME).then(c=>c.addAll(FILES))
+);
 });
+
 self.addEventListener("fetch",e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+e.respondWith(
+ caches.match(e.request).then(r=>r||fetch(e.request))
+);
 });
